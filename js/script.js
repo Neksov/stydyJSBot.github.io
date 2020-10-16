1ef03c07e8a5a06911818ab8ad350b6341db4353
Nekrasov Vadim <n2v@list.ru>
2020-10-16 09:53:28 +0300

    refactor: Добавил стили к кнопки и пдключил ее

--- js/script.js
+++ js/script.js
@@ -12,8 +12,10 @@ function start(){
     let say = prompt('Угадай число от 1 до 100');
   if(isNumber(numberRandom)){ 
     if(say > numberRandom){
+
     say = prompt('Повторите');
     alert('Загаданное число меньше');
+
     }else if(say < numberRandom){
     say = prompt('Повторите');
     alert('Загаданное число больше');
@@ -25,4 +27,3 @@ function start(){
   }
   sayNumber();
 };
-start();
