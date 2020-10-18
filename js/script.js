28217dd52e1ab3e0dc140dbee374f358d92130b6
Nekrasov Vadim <n2v@list.ru>
2020-10-18 22:12:16 +0300

    refactor: Обновил if

--- js/script.js
+++ js/script.js
@@ -1,44 +1,53 @@
 'use strict';
 
-let isNumber = function(n){ //проверка входящих prompt
+let isNumber = function (n) { //проверка входящих prompt
   return !isNaN(parseFloat(n)) && isFinite(n)
 };
-let numberRandom = parseInt(Math.random()*100);//загаданное число
+
+let numberRandom = parseInt(Math.random() * 100);//загаданное число
 console.log(numberRandom);
 
-function start(){
+function start() {
   let say = prompt('Угадай число от 1 до 100');
+  function sayNumber() {
+    if (isNumber(say)) {
+      if (say > numberRandom) {
+        alert('Загаданное число меньше');
+        repeat();
+        alert('Правильное число: ' + numberRandom);
+      } else if (say < numberRandom) {
+        alert('Загаданное число больше');
+        repeat();
+        alert('Правильное число: ' + numberRandom);
+      } else if (say == numberRandom) {
+        alert('Поздравляю, Вы угадали!!! Загаданное число ' + numberRandom);
+      }
+      } else if (say != 'number') {
+        alert('Введите число');
+        repeat();
+      }
+    else (say === null)
+      alert('Игра окончена.');
+    };
 
-  function sayNumber(){
-  if(isNumber(say)){ 
-    if(say > numberRandom){
-      alert('Загаданное число меньше');
-      repeat();
-      alert('Правильное число: ' + numberRandom);
-    }else if(say < numberRandom){
-      alert('Загаданное число больше');
-      repeat();
-      alert ('Правильное число: ' + numberRandom);    
-    }else if(say == numberRandom){
-      alert('Поздравляю, Вы угадали!!!Закаданное число ' + numberRandom);
-    }
-  }else if (say !== 'number'){
-    alert('Введите число');
-    repeat();
-  }else (say === null)
-    alert('Игра окончена');
+  sayNumber();
 };
 
-sayNumber();
+function repeat() {//функция повтора
+  // for (let i = 0; i < 10; i++) {
+    let say = prompt('Повторите');
+  if (say > numberRandom) {
+    alert('Загаданное число меньше');
+  } else if (say < numberRandom) {
+    alert('Загаданное число больше');
+  } else if (say == numberRandom) {
+    alert('Поздравляю, Вы угадали!!! Закаданное число ' + numberRandom);
+  // break;
+  }
+  // say += i;
+  // }
 };
 
-function repeat(){//функция повтора
-  let say = prompt('Повторите');
-    if(say > numberRandom){
-      alert('Загаданное число меньше');
-    }else if(say < numberRandom){
-      alert('Загаданное число больше');
-    }else if(say == numberRandom){
-      alert('Поздравляю, Вы угадали!!!Закаданное число ' + numberRandom);
-    }
-};
+// else if (say != 'number') {
+// alert('Введите число');
+// }
